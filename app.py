d17a2345b4563f43e091cb80d57c40a99080a234
SalvoBevi <salvo.bevi@gmail.com>
2020-10-11 16:53:20 +0000

    Add code for recall from database

--- app.py
+++ app.py
@@ -32,7 +32,14 @@ def get_skills():
 
 @app.route("/add_recipe")
 def add_recipe():
-    return render_template("add_recipe.html")
+    dish = mongo.db.dish.find().sort("dish_name", 1)
+    return render_template("add_recipe.html", dish=dish)
+
+
+@app.route("/ingredients")
+def ingredients():
+    ingredients = mongo.db.ingredients.find().sort("ingredients", 1)
+    return render_template("add_recipe.html", ingredients=ingredients)
 
 
 @app.route("/register", methods=["GET", "POST"])
